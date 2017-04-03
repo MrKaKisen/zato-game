dd67e9872486f612050b024e71b102c8dfc92586
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2017-04-03 21:47:39 +0200

    Add more setup functions

--- zato/lib/setup.lua
+++ zato/lib/setup.lua
@@ -4,4 +4,8 @@
 function setup_love()
   love.window.setTitle("Zato")
   love.window.setMode(1280, 720, {resizable=false, vsync=true})
+  love.window.setDisplaySleepEnabled(false)
+  -- add later on when an icon is made
+  -- local iconImage = love.graphics.newImage("assets/img/logos/icon.png")
+  -- love.window.setIcon(iconImage)
 end
