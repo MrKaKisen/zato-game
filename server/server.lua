50eb642f7d34b0cdf34f762bb2a92eb74ad82d48
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2017-04-04 19:14:08 +0200

    Moved server to https://github.com/MrKaKisen/zato-servers

--- server/server.lua
+++
@@ -1,8 +0,0 @@
--- Server program for Zato
-
--- static vars, added on a per node basis
-serverStatic = {}
-serverStatic.id = 0
-serverStatic.niceName = "Example"
-serverStatic.location = "EU/SE"
-serverStatic.api_key = ""
