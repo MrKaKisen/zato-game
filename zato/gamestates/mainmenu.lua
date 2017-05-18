3c2378efcc290a83d21a465b92cf0e1d2d0831d5
BuildTools <vpytkanal.sverige@gmail.com>
2017-05-18 19:18:26 +0200

    Add empty functions for mainemnu & serverselect

--- zato/gamestates/mainmenu.lua
+++ zato/gamestates/mainmenu.lua
@@ -1,3 +1,13 @@
 -- zato
 -- mainmenu
 -- runs when in the mainmenu
+
+function mainmenu_update(dt)
+  -- love update when gamestate is in mainmenu
+
+end
+
+function mainmenu_draw()
+  -- love draw when gamestate is in mainmenu
+
+end
--- zato/gamestates/serverselect.lua
+++ zato/gamestates/serverselect.lua
@@ -1,3 +1,13 @@
 -- zato
 -- serverselect
 -- runs when in the serverselect
+
+function serverselect_update(dt)
+  -- love update when gamestate is in serverselect
+
+end
+
+function serverselect_draw()
+  -- love draw when gamestate is in serverselect
+
+end
